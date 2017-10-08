c1d1952e5c58b1c7ad6d1eab921d55c7f57458e3
INSPIRON15\baotr_000 <nqkhanh291996@gmail.com>
2017-10-09 01:09:57 +0700

    Update new feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get     "login"    => "sessions#new"
+  post    "login"    => "sessions#create"
+  get  "logout"   => "sessions#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
+  resources :users
   resources :about, only:[:index]
   resources :albums, only: [:index, :show]
   resources :blog, only: [:index]
